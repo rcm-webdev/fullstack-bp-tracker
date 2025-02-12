dd1a2461698a606c071f7cd080141a8787e057d2
rcm-webdev <rcm.webdev.pro@gmail.com>
2025-02-12 11:52:02 -0800

    import passport strategy

--- backend/app.js
+++ backend/app.js
@@ -1,5 +1,10 @@
 const express = require("express");
+const passport = require("passport");
+require("./config/passport")(passport); // 👈🏽 load passport config
 
 const app = express();
+
+app.use(express.json());
+app.use(passport.initialize());
 
 module.exports = app;

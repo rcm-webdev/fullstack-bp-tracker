d81016e476af7c66435cb4c2b636a706685f2e87
rcm-webdev <rcm.webdev.pro@gmail.com>
2025-02-13 13:36:51 -0800

    update user model

--- backend/models/User.js
+++ backend/models/User.js
@@ -1,11 +1,7 @@
-// connect to db
 const mongoose = require("mongoose");
 
-//create a schema for user
-const userSchema = new mongoose.Schema({
-  //take in email
+const UserSchema = new mongoose.Schema({
   email: { type: String, required: true, unique: true },
-  createdAt: { type: Date, default: Date.now },
 });
 
-module.exports = mongoose.model("User", userSchema);
+module.exports = mongoose.model("User", UserSchema);

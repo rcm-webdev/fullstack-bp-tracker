4e85c37dd374b14cc682e47aeb8cf250f5475c53
rcm-webdev <rcm.webdev.pro@gmail.com>
2025-02-13 13:50:20 -0800

    update app.js

--- backend/app.js
+++ backend/app.js
@@ -1,10 +1,38 @@
 const express = require("express");
+const session = require("express-session");
 const passport = require("passport");
-require("./config/passport")(passport); // 👈🏽 load passport config
+const cors = require("cors");
+require("dotenv").config();
+require("./config/passport")(passport);
+
+const authRoutes = require("./routes/auth");
 
 const app = express();
 
+//allow requests from frontend
+app.use(
+  cors({
+    origin: "http://localhost:5173",
+    credentials: true,
+  })
+);
+
 app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+//session setup
+app.use(
+  session({
+    secret: process.env.SESSION_SECRET,
+    resave: false,
+    saveUninitialized: false,
+  })
+);
+
 app.use(passport.initialize());
+app.use(passport.session());
+
+//setup routes for listening
+app.use("/auth", authRoutes);
 
 module.exports = app;

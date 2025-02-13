74ded4d8b5354f7784534b19f827368809f2e1a0
rcm-webdev <rcm.webdev.pro@gmail.com>
2025-02-13 13:50:03 -0800

    setup middleware

---
+++ backend/middleware/auth.js
@@ -0,0 +1,7 @@
+// backend/middleware/auth.js
+module.exports = function ensureAuthenticated(req, res, next) {
+  if (req.isAuthenticated()) {
+    return next();
+  }
+  res.status(401).json({ message: "Unauthorized" });
+};
